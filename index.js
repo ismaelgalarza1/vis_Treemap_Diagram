11105953e4f59cccc7d091033b245786efbaa87c
Ismael G <underthehaze13@outlook.com>
2024-01-29 20:39:52 -0800

    added JSON to URL variables

---
+++ index.js
@@ -0,0 +1,5 @@
+//API URL 
+let kickStarterURL = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/kickstarter-funding-data.json'
+let movieSalesURL ='https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/movie-data.json'
+let videoGameSalesURL = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/video-game-sales-data.json'
+
